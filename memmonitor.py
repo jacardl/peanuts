88662e04a70373ef4bc8d03a84ff501f379f5739
jacardl <u_jar@126.com>
2016-04-15 19:20:12 +0800

    1. fix bug in memmonitor

--- memmonitor.py
+++ memmonitor.py
@@ -217,18 +217,17 @@ class MemMonitorXlsx(threading.Thread):
 
     def run(self):
         self.running = True
-        self.daemon = threading.Thread(target=daemonKernelMonitor, args=(self.terminal, self.interval, self.count,
+        daemon = threading.Thread(target=daemonKernelMonitor, args=(self.terminal, self.interval, self.count,
                                                                     self.file, self.sheetDaemon, self.sheetKernel))
         # daemon.setDaemon(True)
-        self.daemon.start()
+        daemon.start()
         while self.running and self.daemon.isAlive():
-            self.daemon.join(1)
+            daemon.join(1)
         memDiffCalc(self.file, [self.sheetDaemon, self.sheetKernel])
 
         self.stop()
 
     def stop(self):
-        self.daemon.__stop()
         self.running = False
         self.terminal.close()
 
